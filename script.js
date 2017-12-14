8559bf3c476f4766715cf9c2272a206dbe99c105
Jason Giang <jagiang3@s.sfusd.edu>
2017-12-14 01:13:41 +0000

    html and stuff

--- script.js
+++ script.js
@@ -7,30 +7,41 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 
 $(document).ready(function() {
-
+    var input = ("input").val();
     // This function should return true if the input word starts with a vowel,
     // otherwise it should return false.
     function wordStartsWithVowel(word) {
-
+        if(input.charAt(0) === "a e i o u"){
+            return true
+        }else{
+            return false
+        }
     }
-
     // Appends "yay" to the end of the word and returns the word.
     function appendYayToWord(word) {
-
+        $(input).append("yay");
+        return input
     }
-
     // Moves the first consonant to the end of the word, appends "ay" to the end of the word,
     // and returns the word.
     function convertWordWithConsonant(word) {
-
+        $(input).append("ay")
+        return input
     }
-
     // If the word starts with a vowel, return the result of appendYayToWord.
     // Otherwise, return the result of convertWordWithConsonant.
     function convertWordToPigLatin(word) {
-
+        if(wordStartsWithVowel === true){
+            appendYayToWord
+            return input
+        }else if(wordStartsWithVowel === true){
+            convertWordWithConsonant
+            return input
+        }
     }
-
+    $("button").click(function(){
+    console.log("input");
+    }
     // Create a click handler that takes is triggered when the user clicks the translate button.
         // Gets a word from the input box
         // Transforms the word to pig latin
