64e36b84df307c769f0940e5519fde86ec6fb76e
Jason Giang <jagiang3@s.sfusd.edu>
2018-01-11 01:17:09 +0000

    function

--- script.js
+++ script.js
@@ -7,26 +7,39 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 
 $(document).ready(function() {
-    var input = ("input").val();
+    var input = $("#word").val(); 
+    $("#translate").click(function(){
+    $("#word").val(); 
+    console.log("#word");
+    });
     // This function should return true if the input word starts with a vowel,
     // otherwise it should return false.
-    function wordStartsWithVowel(word) {
-        if(input.charAt(0) === "a e i o u"){
-            return true
-        }else{
-            return false
-        }
+    function wordStartsWith(word) {
+    return word.charAt(0);
     }
+    function wordStartsWithVowel(word) {
+    return word.charAt(0) === "a" || 
+           word.charAt(0) === "e" || 
+           word.charAt(0) === "i" || 
+           word.charAt(0) === "o" || 
+           word.charAt(0) === "u";
+    }
+    if(wordStartsWithVowel(testWord1) === true){
+    //  console.log("true");
+    //  } else{
+    //    console.log("false");
+    //}
+
     // Appends "yay" to the end of the word and returns the word.
     function appendYayToWord(word) {
-        $(input).append("yay");
-        return input
+    return word + "yay";
     }
     // Moves the first consonant to the end of the word, appends "ay" to the end of the word,
     // and returns the word.
     function convertWordWithConsonant(word) {
-        $(input).append("ay")
-        return input
+    var other = word.substring(1);
+    var first = word.charAt(0);
+    return other+ first+"ay";
     }
     // If the word starts with a vowel, return the result of appendYayToWord.
     // Otherwise, return the result of convertWordWithConsonant.
@@ -39,11 +52,9 @@ $(document).ready(function() {
             return input
         }
     }
-    $("button").click(function(){
-    console.log("input");
-    }
     // Create a click handler that takes is triggered when the user clicks the translate button.
         // Gets a word from the input box
         // Transforms the word to pig latin
         // Displays the result on the screen in the output element
+
 });
